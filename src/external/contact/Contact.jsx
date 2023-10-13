60b7c7ef957b5f8be0009eace7c4e72789ae6681
vaishnavi1902 <vaishnavikumbhakarna467@gmail.com>
2023-10-13 17:27:58 +0530

    changes

--- src/external/contact/Contact.jsx
+++ src/external/contact/Contact.jsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
 import "./contact.css";
 import { AiOutlineRight } from "react-icons/ai";
-import { BsFillPersonFill } from "react-icons/bs";
-
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 const Contact = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [mobile, setMobile] = useState("");
-  const [message, setMessage] = useState("");
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_mhaayfn', 'template_jk4ofbt', form.current, 'BH9Z0LNh0LZ_erOQF')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  };
   return (
     <>
       <div className="linktag">
@@ -16,54 +24,19 @@ const Contact = () => {
         <a href="/">Contact Us</a>
       </div>
       <section className="contact-section">
-        <div className="heading">
-          <h2>Contact Us</h2>
-        </div>
-        <div className="border"></div>
-        <form className="contact-form" action="">
-          <label htmlFor="name" className="contact-label">
-            Name
-          </label>
-
-          <input
-            icon={<BsFillPersonFill style={{ color: "black" }} />}
-            type="text"
-            placeholder="Your name"
-            value={name}
-            className="contact-form-text"
-            onChange={(e) => setName(e.target.value)}
-          />
-          <label htmlFor="email" className="contact-label">
-            Email
-          </label>
-          <input
-            type="text"
-            placeholder="Your email"
-            value={email}
-            className="contact-form-text"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <label htmlFor="mobile" className="contact-label">
-            Mobile
-          </label>
-          <input
-            type="number"
-            placeholder="Your Mobile"
-            value={mobile}
-            className="contact-form-text"
-            onChange={(e) => setMobile(e.target.value)}
-          />
-          <label htmlFor="message" className="contact-label">
-            Message
-          </label>
-          <textarea
-            type="text"
-            placeholder="Your message"
-            value={message}
-            className="contact-form-text"
-            onChange={(e) => setMessage(e.target.value)}
-          ></textarea>
-          <button className="contact-form-btn">Submit</button>
+        <div className="heading"><h2>Contact Us</h2></div>
+        <form className="contact-form" action="" ref={form} onSubmit={sendEmail}>
+          <label htmlFor="name" className="contact-label"> Name </label>
+          <input type="text" placeholder="Enter Your name" name="name" className="contact-form-text" required/>
+          <label htmlFor="email" className="contact-label"> Email </label>
+          <input type="text" placeholder="Enter Your email" name="email" className="contact-form-text" required />
+          <label htmlFor="mobile" className="contact-label"> Mobile (Optional) </label>
+          <input type="number" placeholder="Enter Your Mobile Number" name="mobile" className="contact-form-text" />
+          <label htmlFor="message" className="contact-label"> Message </label>
+          <textarea type="text" placeholder="Enter Your message" name="message" className="contact-form-text" required></textarea>
+          <div className="flex">
+          <button className='contact-form-btn' type='submit'>Send Message</button>
+          </div>
         </form>
       </section>
     </>
