c0de5b6ddb1e008f39e263d7146274d859a6c6ac
vishakha kasture <vishakhakasture302002@gmail.com>
2023-10-25 23:34:23 +0530

    slider

--- src/external/Courses/Courses.jsx
+++ src/external/Courses/Courses.jsx
@@ -1,9 +1,72 @@
-
+import "./courses.css";
+import { AiOutlineRight } from "react-icons/ai";
+import image from "./../../assets/bg-3.jpg";
 
 const Courses = () => {
   return (
-    <section className="container" id="courses">Courses</section>
-  )
-}
+    <>
+      <div className="linktag">
+        <a href="/">Home </a>{" "}
+        <AiOutlineRight style={{ height: "12px", width: "12px" }} />
+        <a href="/">Courses</a>
+      </div>
+      <section className="container" id="courses">
+        <div className="courses-container">
+          <div className="course-item">
+            <div className="course-image">
+              <img src={image} alt="" />
+            </div>
+            <div className="course-content">
+              <h4 className="course-title">Title</h4>
+              <p className="course-description">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatum quia ea placeat reprehenderit eligendi exercitationem
+                labore ex natus vero est laboriosam illum, tempore nostrum aut
+                provident explicabo, commodi architecto autem!
+              </p>
+              <a href="/" className="start-learning">
+                Start Learning
+              </a>
+            </div>
+          </div>
+          <div className="course-item">
+            <div className="course-image">
+              <img src={image} alt="" />
+            </div>
+            <div className="course-content">
+              <h4 className="course-title">Title</h4>
+              <p className="course-description">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatum quia ea placeat reprehenderit eligendi exercitationem
+                labore ex natus vero est laboriosam illum, tempore nostrum aut
+                provident explicabo, commodi architecto autem!
+              </p>
+              <a href="/" className="start-learning">
+                Start Learning
+              </a>
+            </div>
+          </div>
+          <div className="course-item">
+            <div className="course-image">
+              <img src={image} alt="" />
+            </div>
+            <div className="course-content">
+              <h4 className="course-title">Title</h4>
+              <p className="course-description">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatum quia ea placeat reprehenderit eligendi exercitationem
+                labore ex natus vero est laboriosam illum, tempore nostrum aut
+                provident explicabo, commodi architecto autem!
+              </p>
+              <a href="/" className="start-learning">
+                Start Learning
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Courses
+export default Courses;
